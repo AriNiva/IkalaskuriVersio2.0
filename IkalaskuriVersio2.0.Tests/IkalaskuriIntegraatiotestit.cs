c67c2f737de4ecb0677433ef5a515d0635153e5f
AriNiva <arin.postiloota@outlook.com>
2025-05-30 17:46:19 +0300

    Lisätty yksinkertainen integraatiotesti.

---
+++ IkalaskuriVersio2.0.Tests/IkalaskuriIntegraatiotestit.cs
@@ -0,0 +1,35 @@
+﻿
+using IkalaskuriVersio2._0.Services;
+using Moq;
+
+namespace IkalaskuriVersio2._0.Tests
+{
+    public class IkalaskuriIntegraatiotestit
+    {
+        /* Integraatiotestaus: 
+           * Testaa useiden komponenttien (ei yksittäisten metodien) yhdessä toimimista
+           * Simuloi todellista käyttötilannetta usean vaiheen läpi.*/
+
+        // Testaa Suorita-metodin läpimenon.
+        [Fact]
+        public void Suorita_KokoProsessi_TuottaaOikeatTulosteet()
+        {
+            // Arrange
+            var mockUI = new Mock<IKayttoliittyma>();
+            var palvelu = new IkalaskuriService(mockUI.Object);
+
+            var syotteet = new Queue<string>(["Nainen", "01.01.2000"]);
+
+            mockUI.Setup(ui => ui.LueSyote()).Returns(() => syotteet.Dequeue());
+
+            // Act
+            palvelu.Suorita();
+
+            // Assert
+            mockUI.Verify(ui => ui.Tulosta("Kerro sukupuolesi. (Mies / Nainen)."), Times.Once());
+            mockUI.Verify(ui => ui.Tulosta("Anna syntymäaika muodossa PP.KK.VVVV."), Times.Once());
+            mockUI.Verify(ui => ui.Tulosta(It.Is<string>(s => s.Contains("Odotettua elinaikaa jäljellä"))), Times.Once());
+        }
+
+    }
+}
--- IkalaskuriVersio2.0/Services/IkalaskuriService.cs
+++ IkalaskuriVersio2.0/Services/IkalaskuriService.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace IkalaskuriVersio2._0.Services
 {
     internal class IkalaskuriService
